21f60290b0dc5efd37aca1e93c182fa69fd19588
David Agudelo <david.agudeloo@udea.edu.co>
2025-04-11 22:28:24 -0500

    se realiza la conexion entre el back y el front

--- src/main/java/com/udea/bancoudea/controller/CustomerController.java
+++ src/main/java/com/udea/bancoudea/controller/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/customers")
 public class CustomerController {
     private final CustomerService customerFacade;
